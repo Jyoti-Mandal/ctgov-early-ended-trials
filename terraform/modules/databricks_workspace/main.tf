a9fbf87a1a3fc50ad83ecf0793e4e3a6dc61f580
Jyoti <41127547+Jyoti-Mandal@users.noreply.github.com>
2026-08-19 17:43:17 +0530

    Add files via upload

--- terraform/modules/databricks_workspace/main.tf
+++ terraform/modules/databricks_workspace/main.tf
@@ -1,1 +1,11 @@
+resource "azurerm_databricks_workspace" "main" {
+  name                        = "${var.project}-${var.environment}-dbx"
+  resource_group_name         = var.resource_group_name
+  location                    = var.location
+  sku                         = "premium"   # Required for Unity Catalog
 
+  # Databricks creates a managed resource group for cluster VMs etc.
+  managed_resource_group_name = "${var.project}-${var.environment}-dbx-managed-rg"
+
+  tags = var.tags
+}
---
+++ terraform/modules/databricks_workspace/outputs.tf
@@ -0,0 +1,3 @@
+output "workspace_id"           { value = azurerm_databricks_workspace.main.workspace_id }
+output "workspace_url"          { value = azurerm_databricks_workspace.main.workspace_url }
+output "workspace_resource_id"  { value = azurerm_databricks_workspace.main.id }
